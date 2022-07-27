380eb97b7631ee48a527f269816ba89d9d211379
LuisMarquez95 <50773545+LuisMarquez95@users.noreply.github.com>
2022-07-26 23:51:55 -0500

    Rutas de navegacion

--- src/app/App.js
+++ src/app/App.js
@@ -1,91 +1,38 @@
-import React, {Component} from "react";
-class APP extends Component{
-    constructor(){
-        super();
-        this.state = {
-            title: '',
-            description: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.addGame = this.addGame.bind(this);
-    }
-    addGame(e){
-        fetch('api/task', {
-            method: 'POST',
-            body: JSON.stringify(this.state),
-            headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json'
-            }
-        })
-        .then(res => console.log(res))
-        .then(data => {
-            M.toast({html: 'Juego guardado'});
-            this.setState({title: "", description: ""});
-        })
-        .catch(err => console.error(err));
-
-        e.preventDefault();
-    }
-    componentDidMount(){
-        this.fetchGames();
-    }
-    fetchGames(){
-        fetch('api/task')
-        .then(res => res.json())
-        .then(data => console.log(data));
-    }
-    handleChange(e){
-        const {name, value} = e.target;
-        this.setState({
-            [name] : value 
-        })
-        console.log(e.target.name);
-
-    }
-    render(){
-        return (
-            <div>
-                {/*  NAVIGATION*/}
-                <nav className="light-blue darken-4">
-                    <div className="container">
-                        <a className="brand-logo" href="/"> HOVA VIDEO GAME TEST</a>
-                    </div>
-                </nav>
-                <div className="row">
-                    <div className="col s5">
-                        <div className="card">
-                            <div className="card-content">
-                                <form onSubmit={this.addGame}>
-                                    <div className="row">
-                                        <div className="input-field col s12">
-                                            <i className="material-icons prefix">airplay</i>
-                                            <input name="title" type="text" onChange={this.handleChange} className="autocomplete" value={this.state.title}/>
-                                        </div>
-                                        <div className="input-field col s12">
-                                            <i className="material-icons prefix">border_color</i>
-                                            <input name="description" type="text" onChange={this.handleChange} className="autocomplete" value={this.state.description}/>
-                                        </div>
-                                        <div className="input-field col s12">
-                                            <button type="submit">
-                                                <a className="waves-effect waves-light btn-large"><i className="material-icons right">cloud</i>Salvar</a>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                            <div>
-                                
-                            </div>
-                                <table>
-                                </table>  
-
-                        </div>
-                    
-                    </div>
-                </div>
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import DevE from "./pages/devAdd";
+import Home from './pages/Home'
+export default function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <nav>
+            <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Video Game-Test Hova</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li> <Link to="/" >DashBoard</Link></li>
+                <li><Link to="/DevE">Agregar Desarrollador</Link></li>
+                
+            </ul>
             </div>
-        )
-    }
+        </nav>
+      
+
+        {}
+        <Switch>
+          <Route exact path="/" component={Home}>
+            <Home />
+          </Route>
+          <Route path="/DevE" component={DevE}>
+            <DevE />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-export default APP;
---
+++ src/app/pages/devAdd.js
@@ -0,0 +1,83 @@
+import React, {Component} from "react";
+class DevE  extends Component{
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            description: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.addGame = this.addGame.bind(this);
+    }
+    addGame(e){
+        fetch('api/task', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            }
+        })
+        .then(res => console.log(res))
+        .then(data => {
+            M.toast({html: 'Juego guardado'});
+            this.setState({title: "", description: ""});
+        })
+        .catch(err => console.error(err));
+
+        e.preventDefault();
+    }
+    componentDidMount(){
+        this.fetchGames();
+    }
+    fetchGames(){
+        fetch('api/task')
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }
+    handleChange(e){
+        const {name, value} = e.target;
+        this.setState({
+            [name] : value 
+        })
+        console.log(e.target.name);
+
+    }
+    render(){
+        return (
+            <div>
+               
+                <div className="row">
+                    <div className="col s5">
+                        <div className="card">
+                            <div className="card-content">
+                                <form onSubmit={this.addGame}>
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <i className="material-icons prefix">airplay</i>
+                                            <input name="title" type="text" onChange={this.handleChange} className="autocomplete" value={this.state.title}/>
+                                        </div>
+                                        <div className="input-field col s12">
+                                            <i className="material-icons prefix">border_color</i>
+                                            <input name="description" type="text" onChange={this.handleChange} className="autocomplete" value={this.state.description}/>
+                                        </div>
+                                        <div className="input-field col s12">
+                                            <button type="submit">
+                                                <a className="waves-effect waves-light btn-large"><i className="material-icons right">cloud</i>Salvar</a>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div>
+                                
+                            </div>
+                        </div>
+                    
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export  default DevE
